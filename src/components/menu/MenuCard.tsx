69a72729b87f50d2fccaf3353f7c838d90270c7e
Thomas Woroniak <thomasworoniak@gmail.com>
2026-02-26 14:26:05 -0600

    add images and theme styles

--- src/components/menu/MenuCard.tsx
+++ src/components/menu/MenuCard.tsx
@@ -2,20 +2,23 @@ import styles from './MenuCard.module.scss';
 import type { MenuItem } from '../../types/menu';
 import { formatMoney } from '../../utils/money';
 
-type Props = {
-  item: MenuItem;
-  onClick: () => void;
-};
-
-function tagLabel(t: string) {
-  // simple display for badges
-  return t;
-}
+type Props = { item: MenuItem; onClick: () => void };
 
 export default function MenuCard({ item, onClick }: Props) {
   return (
     <button type='button' className={styles.card} onClick={onClick}>
-      <div className={styles.top}>
+      {item.image ? (
+        <div className={styles.thumb}>
+          <img
+            src={item.image.src}
+            alt={item.image.alt}
+            loading='lazy'
+            decoding='async'
+          />
+        </div>
+      ) : null}
+
+      <div className={styles.content}>
         <div className={styles.nameRow}>
           <span className={styles.name}>{item.name}</span>
           <span className={styles.price}>{formatMoney(item.price)}</span>
@@ -25,14 +28,14 @@ export default function MenuCard({ item, onClick }: Props) {
           <div className={styles.tags} aria-label='Dietary tags'>
             {item.tags.map((t) => (
               <span key={t} className={styles.tag}>
-                {tagLabel(t)}
+                {t}
               </span>
             ))}
           </div>
         ) : null}
-      </div>
 
-      <p className={styles.desc}>{item.description}</p>
+        <p className={styles.desc}>{item.description}</p>
+      </div>
     </button>
   );
 }
